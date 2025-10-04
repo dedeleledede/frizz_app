66ac7a59e3e604434f9238db60b67a5bd915dd75
thispedroo <jf.pedro09@gmail.com>
2025-10-03 21:53:28 -0300

    Funcionamento do ping

--- cogs/ping.py
+++ cogs/ping.py
@@ -5,13 +5,15 @@ import secrets
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='-', intents=intents)
-
-@bot.command()
-async def ping(ctx):
-    latency_sec = bot.latency
-    latency_ms = round(latency_sec * 1000)
-    await ctx.send(f'**Ping!**\nMeu ping está em {latency_ms} ms')
+class PingCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def ping(self, ctx):
+        latency_sec = self.bot.latency
+        latency_ms = round(latency_sec *1000)
+        await ctx.send(f'**Ping!**\nMeu ping está em {latency_ms} ms.') 
+        
+async def setup(bot: commands.Bot):
+    await bot.add_cog(PingCog(bot))
