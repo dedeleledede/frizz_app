3f69e2b0356c7be08c48a24ce35c6b1285d29a51
thispedroo <jf.pedro09@gmail.com>
2025-10-03 21:32:37 -0300

    Comando de ping

--- cogs/ping.py
+++ cogs/ping.py
@@ -5,3 +5,13 @@ import secrets
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    latency_sec = bot.latency
+    latency_ms = round(latency_sec * 1000)
+    await ctx.send(f'**Ping!**\nMeu ping está em {latency_ms} ms')
